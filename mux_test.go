12fd8174709e278b48755564fd2971e19a1b9bd0
sema <semenpermogorov@gmail.com>
2025-01-20 07:43:16 +0300

    v0.0.4

--- mux_test.go
+++ mux_test.go
@@ -15,7 +15,7 @@ func TestSample(t *testing.T) {
 
 func BenchmarkSample(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		if x := fmt.Sprintf("%d", 46); x != "46" {
+		if x := fmt.Sprintf("%d", 49); x != "49" {
 			b.Fatalf("Unexpected string: %s", x)
 		}
 	}
